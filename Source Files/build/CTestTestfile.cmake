1a2f0429889bf81f5d67bed6474bca48a12e4e6a
Michael <u21546551@tuks.co.za>
2023-10-17 16:48:17 +0200

    Delete Source Files/build directory

--- Source Files/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/jmood/Desktop/University/Second Year/COS 214/COS-214-Project/Source Files
-# Build directory: /mnt/c/Users/jmood/Desktop/University/Second Year/COS 214/COS-214-Project/Source Files/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/mnt/c/Users/jmood/Desktop/University/Second Year/COS 214/COS-214-Project/Source Files/build/tests[1]_include.cmake")
-subdirs("_deps/googletest-build")
--- Source Files/build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/jmood/Desktop/University/Second Year/COS 214/COS-214-Project/Source Files/build/_deps/googletest-src/googletest
-# Build directory: /mnt/c/Users/jmood/Desktop/University/Second Year/COS 214/COS-214-Project/Source Files/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
