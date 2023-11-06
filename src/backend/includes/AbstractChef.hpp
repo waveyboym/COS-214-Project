faad2b40220b4c9e03d8b344a36ba1d7f5b9465c
NicholasJHarvey <123.nharvey@gmail.com>
2023-11-06 12:17:26 +0200

    hpp documentation for AbstractChef and BunChef

--- src/backend/includes/AbstractChef.hpp
+++ src/backend/includes/AbstractChef.hpp
@@ -2,7 +2,7 @@
 *@file AbstractChef.hpp AbstractChef.cpp
 *@class AbstractChef
 *@author Jaide
-*@brief an AbstractChef object
+*@brief an AbstractChef class from which concrete BunChef, Chef, and HeadChef classes inherit
 */
 #ifndef ABSTRACT_CHEF_HPP
 #define ABSTRACT_CHEF_HPP
@@ -12,26 +12,29 @@
 #include "Customer.hpp"
 
 /** 
-*@brief a AbstractChef object
+*@brief an AbstractChef class that, when instantiated, is given a type to indicate specialisation
 */
 class AbstractChef{
     private:
         /** 
-        *@brief stores the next chef in the chain
+        *@brief stores the next chef in the chain of responsibility
         */
         std::shared_ptr<AbstractChef> next;
     
     public:
         /** 
-        *@brief initialises an AbstractChef
-        *@param none
+        *@brief stores the AbstractChef's type
         */
-        AbstractChef(std::string type); 
-
         std::string type;
 
         /** 
-        *@brief handles the creation of a meal
+        *@brief initialises an AbstractChef from a type
+        *@param type the type of chef to initialise: bun chef, chef, or head chef
+        */
+        AbstractChef(std::string type); 
+
+        /** 
+        *@brief handles the creation of a meal from an order by adding relevant 
         *@param order the list of orders to be implemented
         *@param customer the customer the order came from
         *@param meal the meal on which to build
@@ -39,12 +42,17 @@ class AbstractChef{
         virtual std::pair<std::shared_ptr<Meal>, std::shared_ptr<Customer>> makeOrder(std::vector<std::shared_ptr<Order>> order, std::shared_ptr<Customer> customer, std::shared_ptr<Meal> meal) = 0;
 
         /** 
-        *@brief adds a new chef to the chain
+        *@brief adds a new chef to the chain of responsibility
         *@param c the chef to add
+        *@return void
         */
         void add(std::shared_ptr<AbstractChef> c);
 
-
+        /** 
+        *@brief retrieves the next chef in the chain
+        *@param none
+        *@return std::shared_ptr<AbstractChef>
+        */
         std::shared_ptr<AbstractChef> getNext(){
             return this->next;
         }
@@ -53,6 +61,7 @@ class AbstractChef{
         *@brief receives an order to be created
         *@param order the list of orders to be implemented
         *@param customer the customer the order is from
+        *@return void
         */
         void receiveOrder(std::vector<std::shared_ptr<Order>> order, std::shared_ptr<Customer> customer);
 
