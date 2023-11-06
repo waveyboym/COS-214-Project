f393327eb293dc4760378fb683c2d88b957c1570
Remiku-bit <frostfrozen1239@gmail.com>
2023-11-06 17:04:26 +0200

    AddChickenBurgerPatty documentation complete

--- src/backend/includes/AddChickenBurgerPatty.hpp
+++ src/backend/includes/AddChickenBurgerPatty.hpp
@@ -1,20 +1,24 @@
 /**
- * @file AddChickenBurgerPatty.hpp
+ * @file AddChickenBurgerPatty.hpp AddChickenBurgerPatty.hpp
+ * @class AddChickenBurgerPatty
  * @author Lunga
+ * @implements Order
  * @brief AddChickenBurgerPatty, a ConcreteCommand of the Command(Order Class)
- * @version 0.1
- * @date 2023-11-03
- * 
- * @copyright Copyright (c) 2023
- * 
  */
 #ifndef ADDCHICKENBURGERPATTY_HPP
 #define ADDCHICKENBURGERPATTY_HPP
 
 #include "Order.hpp"
-
+/**
+ * @brief AddChickenBurgerPatty class, which inherits from the Order interface.A ConcreteCommand which aides in generating a new ChickenPatty Ingredient.
+ */
 class AddChickenBurgerPatty :public Order{
     public:
+/**
+ * @brief creates a ChickenPatty Ingredient and Passes a std::share_pte<Meal> as the "next" of the "ingredient" of the Meal
+ * @param m 
+ * @return std::shared_ptr<Meal> 
+ */
     std::shared_ptr<Meal> executeOrder(std::shared_ptr<Meal> m);
 };
 
