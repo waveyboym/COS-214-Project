c71839fdca691531310e96ac28d63cad36b8ab83
Remiku-bit <frostfrozen1239@gmail.com>
2023-11-06 17:17:32 +0200

    AddJuice documentation complete

--- src/backend/includes/AddJuice.hpp
+++ src/backend/includes/AddJuice.hpp
@@ -1,20 +1,24 @@
 /**
- * @file AddJuice.hpp
+ * @file AddJuice.hpp AddJuice.cpp
+ * @class AddJuice
  * @author Lunga
+ * @implements Order
  * @brief AddJuice, a ConcreteCommand of the Command(Order Class)
- * @version 0.1
- * @date 2023-11-03
- * 
- * @copyright Copyright (c) 2023
- * 
  */
 #ifndef ADDJUICE_HPP
 #define ADDJUICE_HPP
 
 #include "Order.hpp"
-
+/**
+ * @brief AddJuice class, which inherits from the Order interface.A ConcreteCommand which aides in generating a new Juice Ingredient.
+ */
 class AddJuice : public Order {
     public:
+/**
+ * @brief creates a Juice Ingredient and Passes a std::share_pte<Meal> as the "next" of the "ingredient" of the Meal
+ * @param m 
+ * @return std::shared_ptr<Meal> 
+ */
     std::shared_ptr<Meal> executeOrder(std::shared_ptr<Meal> m);
 };
 
