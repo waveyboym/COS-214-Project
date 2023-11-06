d81f1495d29ce422dcc1cbefbf1010d9e038d95a
NicholasJHarvey <123.nharvey@gmail.com>
2023-11-06 22:23:49 +0200

    .hpp ingredients
    no bun
    normal bun
    water
    tomato

--- src/backend/includes/AddWater.hpp
+++ src/backend/includes/AddWater.hpp
@@ -1,20 +1,25 @@
 /**
- * @file AddWater.hpp
+ * @file AddWater.hpp AddWater.cpp
+ * @class AddWater
  * @author Lunga
+ * @implements Order
  * @brief AddWater, a ConcreteCommand of the Command(Order Class)
- * @version 0.1
- * @date 2023-11-03
- * 
- * @copyright Copyright (c) 2023
- * 
  */
+
 #ifndef ADDWATER_HPP
 #define ADDWATER_HPP
 
 #include "Order.hpp"
-
+/**
+ * @brief AddWater class, which inherits from the Order interface.A ConcreteCommand which aides in generating a new Water Ingredient.
+ */
 class AddWater : public Order {
 public:
+    /**
+    * @brief creates a Water Ingredient and Passes a std::share_pte<Meal> as the "next" of the "ingredient" of the Meal
+    * @param m 
+    * @return std::shared_ptr<Meal> 
+    */
     std::shared_ptr<Meal> executeOrder(std::shared_ptr<Meal> m);
 };
 
