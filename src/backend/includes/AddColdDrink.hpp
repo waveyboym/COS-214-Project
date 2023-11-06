76a43f259e1a7333adb32278ce93bf130ef19f0c
Remiku-bit <frostfrozen1239@gmail.com>
2023-11-06 17:09:28 +0200

    AddColdDrink documentation complete

--- src/backend/includes/AddColdDrink.hpp
+++ src/backend/includes/AddColdDrink.hpp
@@ -1,20 +1,25 @@
 /**
- * @file AddColdDrink.hpp
+ * @file AddColdDrink.hpp AddColdDrink.cpp
+ * @class AddColdDrink
  * @author Lunga
+ * @implements Order
  * @brief AddColdDrink, a ConcreteCommand of the Command(Order Class)
- * @version 0.1
- * @date 2023-11-03
- * 
- * @copyright Copyright (c) 2023
- * 
+ 
  */
 #ifndef ADDCOLDDRINK
 #define ADDCOLDDRINK
 
 #include "Order.hpp"
-
+/**
+ * @brief AddColdDrink class, which inherits from the Order interface.A ConcreteCommand which aides in generating a new ColdDrink Ingredient.
+ */
 class AddColdDrink : public Order {
     public:
+/**
+ * @brief creates a ColdDrink Ingredient and Passes a std::share_pte<Meal> as the "next" of the "ingredient" of the Meal
+ * @param m 
+ * @return std::shared_ptr<Meal> 
+ */
     std::shared_ptr<Meal> executeOrder(std::shared_ptr<Meal>);
 };
 
